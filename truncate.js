7e83b59f88f12da05414316bc2bad97a54d6b45e
jeffreyrichardbigler <jeffreyrichardbigler@gmail.com>
2016-01-22 19:04:22 -0500

    fixed errors in code and added comments

--- truncate.js
+++ truncate.js
@@ -18,7 +18,7 @@
 // are experimenting further or pursuing stretch goals.
 
 
-var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
+var OriginalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
 /*var wordLimit = 8;
 var shortText = truncateWords(originalText, wordLimit);
 console.log('originalText: ' + originalText);
